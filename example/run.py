85ba003e716bdf705a693316c6465e55815cf542
Anton <antojohansson1@gmail.com>
2022-10-07 18:42:56 +0200

    dawd

--- example/run.py
+++ example/run.py
@@ -26,13 +26,48 @@ m, height = make_map(indata)
 pos = height + 1
 print(m)
 
+def illegal(x,y, l):
+    xlen = len(l)
+    ylen = len(l[0])
+    return x >= xlen or y >= ylen or x < 0 or y < 0
+
+def flood_fill(x,y,l):
+    if l[x][y] == 3 or illegal(x,y, l):
+        return
+    l[x][y] = 3
+    
+    perm = [
+        (-1, 0),
+        (1,0),
+        (0,-1)
+        (0,1)
+    ]
+    for p in perm:
+        flood_fill(x + p[0], y + p[1])
+
 def print_found(pos, l):
-    points = indata[pos].split(" ")
-        
-    start = 
+    points = pos.split(" ")
+    sx = int(points[0]) - 1
+    sy = int(points[1]) - 1
+    start = l[sx][sy]
+    end =  l[int(points[2]) - 1][int(points[3]) - 1]
+    
+    msg = ""    
+    if start != end:
+        print("neither")
+        return
+    elif start == 1:
+        msg = "decimal"
+    else:
+        msg = "binary"
 
-    flood_fill(points[0] -1 , points[1] - 1, m)
+    flood_fill(int(points[0]) -1 , int(points[1]) - 1, l)
+    if l[int(points[2]) - 1][int(points[3]) - 1] == 3:
+        print(msg)
+    else:
+        print("neither")
 
+    
 
-for i in range(pos, len(indata)):
-    print_found(indata[i], m.clone())
+for i in range(pos + 1, len(indata)):
+    print_found(indata[i], m.copy())
